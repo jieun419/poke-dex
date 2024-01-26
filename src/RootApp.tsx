5106015513f0e8633adfcdfadfc0e92742177221
jieun419 <crg1050@naver.com>
2024-01-26 20:28:04 +0900

    refactor: import 순서 정리

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,9 +1,9 @@
+import { useSelector } from 'react-redux';
 import App from './App.tsx';
-import GlobalStyle from './styles/GlobalStyles';
-import { ThemeProvider } from 'styled-components';
 import { light, dark } from './styles/theme.ts';
 import { RootState } from './store/index.ts';
-import { useSelector } from 'react-redux';
+import GlobalStyle from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
 
 function RootApp() {
   const themeMode = useSelector((state: RootState) => state.themeType.themeMode);
