1f5b8ef21868bc428b472bd8a41cf650445d7c87
jieun419 <crg1050@naver.com>
2024-01-27 01:47:16 +0900

    feat: 스켈레톤 카드 추가

---
+++ src/components/skeleton/SkeletonCard.tsx
@@ -0,0 +1,51 @@
+import styled, { keyframes } from 'styled-components';
+
+const load = keyframes`
+  100% {
+    background-position: -100% 0;
+  }
+`;
+
+const SkeletonWrap = styled.div`
+  position: relative;
+  overflow: hidden;
+  border-radius: 10px;
+  background: linear-gradient(90deg, #e5e5e5 30%, #f0f0f0 30%, #f0f0f0 30%, #e5e5e5 50%);
+  background-size: 200% 100%;
+  background-position: 100% 0;
+  animation: ${load} 1.5s infinite;
+  /* box-shadow: 4px 4px 20px 0 rgba(0, 0, 0, 0.1); */
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  z-index: 1;
+  cursor: pointer;
+  transition: 0.3s;
+  width: 100%;
+  height: 0;
+  padding-bottom: 100%;
+  background-color: #ccc;
+`;
+
+const SkeletonCard = () => {
+  return (
+    <>
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+      <SkeletonWrap />
+    </>
+  );
+};
+
+export default SkeletonCard;
