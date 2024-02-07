4d8e60de70e8c96a099fc4a86660611fa12f507e
jieun419 <crg1050@naver.com>
2024-02-08 04:23:24 +0900

    feat: 언어 slice 추가

---
+++ src/store/language-slice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type initialStateT = {
+  languageMode: string;
+};
+const toggleLanguageMode = localStorage.getItem('LANGUAGE');
+
+const initialState: initialStateT = {
+  languageMode: toggleLanguageMode ? toggleLanguageMode : 'en',
+};
+
+const languageSlice = createSlice({
+  name: 'languagetype',
+  initialState,
+  reducers: {
+    toggleEnMode(state) {
+      state.languageMode = 'en';
+    },
+    toggleKrMode(state) {
+      state.languageMode = 'kr';
+    },
+  },
+});
+
+export const languageReducer = languageSlice.reducer;
+export const languageActions = languageSlice.actions;
