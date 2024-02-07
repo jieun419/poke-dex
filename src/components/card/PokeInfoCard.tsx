246849eb159970148964033d83d2ea8ddb4c0cda
jieun419 <crg1050@naver.com>
2024-02-08 02:37:43 +0900

    refactor: import 순서 변경

--- src/components/card/PokeInfoCard.tsx
+++ src/components/card/PokeInfoCard.tsx
@@ -1,18 +1,18 @@
 import styled, { css } from 'styled-components';
-import Pokeball from '../../assets/icons/Pokeball';
-import TypeText from '../text/TypeText';
-import PokemonDetail from '../../pages/detail/PokemonDetail';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { overlayMadalActions } from '../../store/overlayModal-slice';
 import { usePokeDetailData } from '../../hooks/services/queries/usePokeDetailData';
+import PokemonDetail from '../../pages/detail/PokemonDetail';
+import Pokeball from '../../assets/icons/Pokeball';
+import TypeText from '../text/TypeText';
 
 const PokeCardContainer = styled.div`
   position: relative;
   overflow: hidden;
   background-color: var(--box-color);
   border-radius: 10px;
-  box-shadow: 4px 4px 20px 0 rgba(234, 231, 231, 0.1);
+  box-shadow: 4px 4px 20px 0 rgba(0, 0, 0, 0.1);
   padding: 20px;
   display: flex;
   flex-direction: column;
@@ -79,9 +79,9 @@ type TypesT = {
   };
 };
 
-type PokeListDataT = {
+interface PokeListDataT {
   name: string;
-};
+}
 
 const PokeInfoCard = ({ name }: PokeListDataT) => {
   const dispatch = useDispatch();
