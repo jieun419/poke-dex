87bae9d526a07927e9075c333557c9f93bfde8d8
jieun419 <crg1050@naver.com>
2024-04-04 19:20:08 +0900

    feat: 불필요한 코드 삭제

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import SearchBox from '../../components/box/SearchBox';
 import MainLogo from '../../components/main/MainLogo';
-import { useRef } from 'react';
 import PokeContList from '../list/PokeContList';
 
 const MainContainer = styled.main`
@@ -13,15 +12,7 @@ const MainContainer = styled.main`
   margin: 0 auto;
 `;
 
-const InfinityDiv = styled.div`
-  height: 50px;
-  width: 100%;
-  background-color: #ff0;
-`;
-
 const Main = () => {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
-
   return (
     <>
       <MainContainer>
@@ -29,7 +20,6 @@ const Main = () => {
         <SearchBox />
         <PokeContList />
       </MainContainer>
-      <InfinityDiv ref={scrollRef} />
     </>
   );
 };
